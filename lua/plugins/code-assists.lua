f70f0db0409cd34162e72d7fd1a72234ded1c47d
arnoldje <john.arnold@lynchburgva.gov>
2026-06-29 16:32:27 -0400

    wip roslyn replaced with roslyn_ls

--- lua/plugins/code-assists.lua
+++ lua/plugins/code-assists.lua
@@ -108,35 +108,35 @@ return {
       },
     },
   },
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = {
-      "kevinhwang91/promise-async",
-    },
-    setup = function()
-      vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-      vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities.textDocument.foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true,
-      }
-      local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
-      for _, ls in ipairs(language_servers) do
-        require("lspconfig")[ls].setup({
-          capabilities = capabilities,
-          -- you can add other fields for setting up lsp server in this table
-        })
-      end
-
-      -- require("ufo").setup({
-      --   provider_selector = function(bufnr, filetype, buftype)
-      --     return { 'treesitter', 'indent' }
-      --   end
-      -- })
-    end,
-  },
+  -- {
+  --   "kevinhwang91/nvim-ufo",
+  --   dependencies = {
+  --     "kevinhwang91/promise-async",
+  --   },
+  --   setup = function()
+  --     vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+  --     vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+  --
+  --     local capabilities = vim.lsp.protocol.make_client_capabilities()
+  --     capabilities.textDocument.foldingRange = {
+  --       dynamicRegistration = false,
+  --       lineFoldingOnly = true,
+  --     }
+  --     local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
+  --     for _, ls in ipairs(language_servers) do
+  --       require("lspconfig")[ls].setup({
+  --         capabilities = capabilities,
+  --         -- you can add other fields for setting up lsp server in this table
+  --       })
+  --     end
+  --
+  --     -- require("ufo").setup({
+  --     --   provider_selector = function(bufnr, filetype, buftype)
+  --     --     return { 'treesitter', 'indent' }
+  --     --   end
+  --     -- })
+  --   end,
+  -- },
   {
     "HiPhish/rainbow-delimiters.nvim"
   },
--- lua/plugins/language-servers.lua
+++ lua/plugins/language-servers.lua
@@ -1,6 +1,10 @@
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
+
 return {
 	{
 		"neovim/nvim-lspconfig",
+    lazy = false,
 	},
 	{
 		"saecki/crates.nvim",
@@ -21,71 +25,67 @@ return {
 	-- 		vim.lsp.enable("jdtls")
 	-- 	end,
 	-- },
-	{
-		"seblyng/roslyn.nvim",
-		ft = { "cs", "razor" },
-		config = function()
-			local mason_registry = require("mason-registry")
-
-			-- local rzls_path = vim.fn.expand("$MASON/packages/rzls/libexec")
-			-- local cmd = {
-			-- 	"roslyn",
-			-- 	"--stdio",
-			-- 	"--logLevel=Information",
-			-- 	"--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-			-- "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
-			-- "--razorDesignTimePath="
-			-- .. vim.fs.joinpath(rzls_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
-			-- "--extension",
-			-- vim.fs.joinpath(rzls_path, "RazorExtension", "Microsoft.VisualStudioCode.RazorExtension.dll"),
-			-- }
-
-			vim.lsp.config("roslyn", {
-				cmd = cmd,
-				-- handlers = require("rzls.roslyn_handlers"),
-				settings = {
-					["csharp|inlay_hints"] = {
-						csharp_enable_inlay_hints_for_implicit_object_creation = true,
-						csharp_enable_inlay_hints_for_implicit_variable_types = true,
-
-						csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-						csharp_enable_inlay_hints_for_types = true,
-						dotnet_enable_inlay_hints_for_indexer_parameters = true,
-						dotnet_enable_inlay_hints_for_literal_parameters = true,
-						dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-						dotnet_enable_inlay_hints_for_other_parameters = true,
-						dotnet_enable_inlay_hints_for_parameters = true,
-						dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-						dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-						dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-					},
-					["csharp|code_lens"] = {
-						dotnet_enable_references_code_lens = true,
-					},
-					["csharp|completion"] = {
-						dotnet_show_name_completion_suggestions = true,
-						dotnet_show_completion_items_from_unimported_namespaces = true,
-					},
-					["csharp|background_analysis"] = {
-						background_analysis = {
-							dotnet_analyzer_diagnostics_scope = "fullSolution",
-							dotnet_compiler_diagnostics_scope = "fullSolution",
-						},
-					},
-				},
-			})
-			vim.lsp.enable("roslyn")
-		end,
-		init = function()
-			-- We add the Razor file types before the plugin loads.
-			vim.filetype.add({
-				extension = {
-					razor = "razor",
-					cshtml = "razor",
-				},
-			})
-		end,
-	},
+	-- {
+	-- 	"seblyng/roslyn.nvim",
+	--    lazy = false,
+	-- 	-- opts = {
+	-- 	--     filewatching = "auto",
+	-- 	--     config = {
+	-- 	--       capabilities = capabilities,
+	-- 	--     }
+	-- 	--   },
+	-- 	config = function()
+	-- 		vim.lsp.config("roslyn", {
+	--        capabilities = {
+	--          workspace = {
+	--            didChangeWatchedFiles = {
+	--              dynamicRegistration = true
+	--            },
+	--          },
+	--        },
+	-- 			settings = {
+	-- 				["csharp|inlay_hints"] = {
+	-- 					csharp_enable_inlay_hints_for_implicit_object_creation = true,
+	-- 					csharp_enable_inlay_hints_for_implicit_variable_types = true,
+	--
+	-- 					csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+	-- 					csharp_enable_inlay_hints_for_types = true,
+	-- 					dotnet_enable_inlay_hints_for_indexer_parameters = true,
+	-- 					dotnet_enable_inlay_hints_for_literal_parameters = true,
+	-- 					dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+	-- 					dotnet_enable_inlay_hints_for_other_parameters = true,
+	-- 					dotnet_enable_inlay_hints_for_parameters = true,
+	-- 					dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+	-- 					dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+	-- 					dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+	-- 				},
+	-- 				["csharp|code_lens"] = {
+	-- 					dotnet_enable_references_code_lens = true,
+	-- 				},
+	-- 				["csharp|completion"] = {
+	-- 					dotnet_show_name_completion_suggestions = true,
+	-- 					dotnet_show_completion_items_from_unimported_namespaces = true,
+	-- 				},
+	-- 				["csharp|background_analysis"] = {
+	-- 					background_analysis = {
+	-- 						dotnet_analyzer_diagnostics_scope = "fullSolution",
+	-- 						dotnet_compiler_diagnostics_scope = "fullSolution",
+	-- 					},
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 		vim.lsp.enable("roslyn")
+	-- 	end,
+	-- 	init = function()
+	-- 		-- We add the Razor file types before the plugin loads.
+	-- 		vim.filetype.add({
+	-- 			extension = {
+	-- 				razor = "razor",
+	-- 				cshtml = "razor",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
