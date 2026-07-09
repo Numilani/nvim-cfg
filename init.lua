68c7778b49bf63118211680ba9880b80b88b69cc
numilani <numilani@protonmail.com>
2026-07-09 15:38:48 -0400

    feat: tweak foldexpr handling

--- init.lua
+++ init.lua
@@ -20,7 +20,6 @@ vim.opt.foldenable = true
 vim.opt.foldlevel = 99
 vim.opt.foldcolumn = "auto"
 vim.opt.foldmethod = "expr"
--- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
 vim.opt.background = "dark"
 
@@ -81,6 +80,18 @@ vim.cmd.colorscheme("molokai")
 --
 -- L S P   A N D   D I A G N O S T I C S
 --
+
+-- handle treesitter vs lsp folding
+
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client:supports_method("textDocument/foldingRange") then
+      vim.wo.foldexpr = "v:lua.vim.lsp.foldexpr()"
+    end
+  end,
+})
 
 -- disable underlines for info/hints 
 vim.diagnostic.config({
