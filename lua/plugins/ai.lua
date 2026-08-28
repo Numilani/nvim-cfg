5b783d7f929330bb607ace059f76630d9b4469e8
numilani <numilani@protonmail.com>
2026-08-28 18:46:34 -0400

    fix codecompanion config

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,12 +1,4 @@
 return {
-	-- {
-	-- 	"CopilotC-Nvim/CopilotChat.nvim",
-	-- 	dependencies = {
-	-- 		{ "nvim-lua/plenary.nvim", branch = "master" },
-	-- 	},
-	-- 	build = "make tiktoken",
-	-- 	opts = {},
-	-- },
 	{
 		"olimorris/codecompanion.nvim",
 		version = "^19.0.0",
@@ -14,84 +6,82 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
-			"zbirenbaum/copilot.lua", -- this is what handles auth instead of declaring a token in the adapters section
+			"zbirenbaum/copilot.lua", -- this is what handles auth instead of declaring a token in the adapters section; comment out if not using copilot
 		},
-		setup = function()
-			requre("codecompanion").setup({
-				interactions = {
-					chat = {
-						adapter = "copilot", -- toggle to "claude_code" for claude
-					},
-					inline = {
-						adapter = "copilot", -- toggle to "anthropic" for claude
-					},
-					cli = {
-						agent = "copilot",
-						agents = {
-							copilot = {
-								cmd = "copilot",
-								args = {},
-								description = "GitHub Copilot CLI",
-								provider = "terminal",
-							},
-							-- claude_code = {
-							-- 	cmd = "claude",
-							-- 	args = {},
-							-- 	description = "Claude Code CLI",
-							-- 	provider = "terminal",
-							-- },
+		opts = {
+			interactions = {
+				chat = {
+					adapter = "copilot", -- toggle to "claude_code" for claude
+				},
+				inline = {
+					adapter = "copilot", -- toggle to "anthropic" for claude
+				},
+				cli = {
+					agent = "copilot",
+					agents = {
+						copilot = {
+							cmd = "copilot",
+							args = {},
+							description = "GitHub Copilot CLI",
+							provider = "terminal",
 						},
+						-- claude_code = {
+						-- 	cmd = "claude",
+						-- 	args = {},
+						-- 	description = "Claude Code CLI",
+						-- 	provider = "terminal",
+						-- },
 					},
 				},
-				adapters = {
-					http = {
-						copilot = function()
-							return require("codecompanion.adapters").extend("copilot_acp", {
-								schema = {
-									model = {
-										default = "auto",
-									},
+			},
+			adapters = {
+				http = {
+					copilot = function()
+						return require("codecompanion.adapters").extend("copilot", {
+							schema = {
+								model = {
+									default = "auto",
 								},
-							})
-						end,
-						-- anthropic = function()
-						-- 	return require("codecompanion.adapters").extend("anthropic", {
-						-- 		env = {
-						-- 			api_key = "ANTHROPIC_API_KEY", -- if your key is already stored in a default ENV variable, comment this out!
-						-- 		},
-						-- 		schema = {
-						-- 			model = { default = "claude-sonnet-4-5" },
-						-- 			extended_thinking = { default = false },
-						-- 		},
-						-- 		opts = {
-						-- 			compaction = true,
-						-- 		},
-						-- 	})
-						-- end,
-					},
-					acp = {
-						copilot_acp = function()
-							return require("codecompanion.adapters").extend("copilot_acp", {})
-						end,
-						-- claude_code = function()
-						-- 	return require("codecompanion.adapters").extend("claude_code", {
-						-- 		env = {
-						-- 			CLAUDE_CODE_OAUTH_TOKEN = "CLAUDE_CODE_OAUTH_TOKEN",
-						-- 			-- ANTHROPIC_API_KEY = "ANTHROPIC_API_KEY"
-						-- 		},
-						-- 		defaults = {
-						-- 			mcpServers = "inherit_from_config",
-						-- 			session_config_options = {
-						-- 				model = function(_)
-						-- 					return "claude-sonnet-4-5"
-						-- 				end,
-						-- 			},
-						-- 		},
-						-- 	})
-						-- end,
-					},
+							},
+						})
+					end,
+					-- anthropic = function()
+					-- 	return require("codecompanion.adapters").extend("anthropic", {
+					-- 		env = {
+					-- 			api_key = "ANTHROPIC_API_KEY", -- if your key is already stored in a default ENV variable, comment this out!
+					-- 		},
+					-- 		schema = {
+					-- 			model = { default = "claude-sonnet-4-5" },
+					-- 			extended_thinking = { default = false },
+					-- 		},
+					-- 		opts = {
+					-- 			compaction = true,
+					-- 		},
+					-- 	})
+					-- end,
 				},
-			})
-		end,
+				acp = {
+					copilot_acp = function()
+						return require("codecompanion.adapters").extend("copilot_acp", {})
+					end,
+					-- claude_code = function()
+					-- 	return require("codecompanion.adapters").extend("claude_code", {
+					-- 		env = {
+					-- 			CLAUDE_CODE_OAUTH_TOKEN = "CLAUDE_CODE_OAUTH_TOKEN",
+					-- 			-- ANTHROPIC_API_KEY = "ANTHROPIC_API_KEY"
+					-- 		},
+					-- 		defaults = {
+					-- 			mcpServers = "inherit_from_config",
+					-- 			session_config_options = {
+					-- 				model = function(_)
+					-- 					return "claude-sonnet-4-5"
+					-- 				end,
+					-- 			},
+					-- 		},
+					-- 	})
+					-- end,
+				},
+			},
+		},
 	},
 }
